cc5394e018a3bc819e1a3e06d03825bdb22dd252
Alexis Barrios <u20181a146@upc.edu.pe>
2022-10-17 04:25:24 -0500

    update env vars

--- src/main/java/com/example/backpacientes/BackPacientesApplication.java
+++ src/main/java/com/example/backpacientes/BackPacientesApplication.java
@@ -4,7 +4,7 @@ import com.example.backpacientes.config.CassandraConfig;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 //@EnableDiscoveryClient
 @EnableRabbit
--- src/main/java/com/example/backpacientes/controller/ServiceInstanceRestController.java
+++ src/main/java/com/example/backpacientes/controller/ServiceInstanceRestController.java
@@ -2,8 +2,8 @@ package com.example.backpacientes.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
+//import org.springframework.cloud.client.ServiceInstance;
+//import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
